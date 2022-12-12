683d71dfe541490f57aff7bcc8fde25667f43d8b
Basmalahasad <asad@hartford.edu>
2022-12-12 19:33:46 +0300

    appended navigation to menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,7 @@
 let modal = document.getElementById(`modal`);
+let menu = document.getElementById(`menu-link`);
+let navigation = document.querySelector(`nav`);
+menu.appendChild(navigation);
 let showModal = () => {
     modal.style.display = `block`;
 };
