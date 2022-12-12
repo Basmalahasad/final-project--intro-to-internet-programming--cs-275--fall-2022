46513b194a40a97a64cc7be046d141293efd517e
Basmalahasad <asad@hartford.edu>
2022-12-12 19:34:10 +0300

    button shows menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,19 @@
 let modal = document.getElementById(`modal`);
 let menu = document.getElementById(`menu-link`);
+let header = document.querySelector(`header`);
+let body = document.querySelector(`body`);
 let navigation = document.querySelector(`nav`);
 menu.appendChild(navigation);
 let showModal = () => {
     modal.style.display = `block`;
+    navigation.style.display = `none`;
+    body.style.overflow = `hidden`;
 };
+let showMenu = () => {
+    navigation.style.display = `block`;
+};
+
 let modalClick =  document.getElementById(`modal-link`);
 modalClick.setAttribute(`onclick`,`showModal();`);
+
+menu.setAttribute(`onclick`,`showMenu();`);
